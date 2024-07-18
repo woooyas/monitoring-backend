cf3a23107b0b391479192492fe713ebd8cbf8dc7
지민 <njmin8@naver.com>
2024-07-18 21:16:02 +0900

    Initial commit

---
+++ src/main/java/org/jimin/mocksensorapi/MockSensorApiApplication.java
@@ -0,0 +1,13 @@
+package org.jimin.mocksensorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockSensorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockSensorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jimin/mocksensorapi/MockSensorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jimin.mocksensorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockSensorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
