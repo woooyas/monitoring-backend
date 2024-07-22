4620c4426434f6367a37f275784cdb4eea7cbb46
지민 <njmin8@naver.com>
2024-07-23 00:22:18 +0900

    feat: MqttConfig 및 dto 구현

---
+++ src/main/java/org/jimin/sensorapi/dto/SensorValue.java
@@ -0,0 +1,14 @@
+package org.jimin.sensorapi.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SensorValue {
+    private Long time;
+    private String value;
+}
